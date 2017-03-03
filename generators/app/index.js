ded708b6cf4b4e05f53c63048f45f962416693b1
Charlie Jackson <charlie@charliejackson.com>
2017-03-03 15:07:21 +0000

    Added basic flow support

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,24 +1,53 @@
 const Generator = require('yeoman-generator');
-const path = require('path');
-const checkOutOfDatePackages = require('check-out-of-date-packages');
+// const path = require('path');
+// const checkOutOfDatePackages = require('check-out-of-date-packages');
+const winston = require('winston');
+const npmAddScript = require('npm-add-script');
 
 module.exports = class extends Generator {
-  initializing() {
-    const cwd = path.join(__dirname, '../../');
-    return checkOutOfDatePackages(cwd, 'Charlie Jackson');
-  }
+  // initializing() {
+  //   const cwd = path.join(__dirname, '../../');
+  //   return checkOutOfDatePackages(cwd, 'Charlie Jackson');
+  // }
 
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('./**/*'),
-      this.destinationPath('./'),
-      { variable: 'value' }
+    this.fs.copy(
+      this.templatePath('./.babelrc'),
+      this.destinationPath('./.babelrc')
     );
 
-    this.fs.copyTpl(
-      this.templatePath('./**/.*'),
-      this.destinationPath('./'),
-      { variable: 'value' }
+    this.fs.copy(
+      this.templatePath('./.flowconfig'),
+      this.destinationPath('./.flowconfig')
     );
+  }
+
+  addLintScript() {
+    const packageJsonPath = this.destinationPath('package.json');
+    let packageJson;
+
+    try {
+      packageJson = this.fs.readJSON(packageJsonPath);
+    } catch (e) {
+      winston.log('info', 'No package.json file, creating one');
+      this.spawnCommandSync('npm', ['init']);
+      packageJson = this.fs.readJSON(packageJsonPath);
+    }
+
+    winston.log('debug', 'packageJson', packageJson);
+
+    if (!packageJson.scripts || !packageJson.scripts.flow) {
+      npmAddScript({ key: 'flow', value: 'flow' });
+    }
+
+    winston.log('debug', 'packageJson', packageJson);
+
+    return true;
+  }
+
+  installPackages() {
+    this.npmInstall(['flow-bin'], { save: true });
+    this.npmInstall(['babel-cli'], { save: true });
+    this.npmInstall(['babel-plugin-transform-flow-strip-types'], { save: true });
   }
 };
